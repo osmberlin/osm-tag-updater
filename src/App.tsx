7ce3b64b0304472ec7f61aec1a07d9f4aed560c3
Tobias <tobias@fixmycity.de>
2022-11-06 09:22:57 +0100

    Minor CSS udpate

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="mx-auto max-w-xl">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
