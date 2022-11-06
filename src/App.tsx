3d099ba26a7b88bb723000aca923b2b4e0dc1949
Tobias <tobias@fixmycity.de>
2022-11-06 08:39:42 +0100

    Setup: TailwindCSS
    via https://tailwindcss.com/docs/guides/vite

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
 
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
