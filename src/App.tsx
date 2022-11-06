b5495a073a2328dd600d203b14dd08a4a9e90e23
Tobias <tobias@fixmycity.de>
2022-11-06 08:42:54 +0100

    Setup: Prettier, TW Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
