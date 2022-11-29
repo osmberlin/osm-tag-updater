f708ac647f772f53f3e67360f3b638a6e1736b49
Tobias <tobias@fixmycity.de>
2022-11-29 05:40:08 +0100

    Rename variable

--- src/pages/PageHome/useOsmQuery/useOsmQueryt.ts
+++ src/pages/PageHome/useOsmQuery/useOsmQueryt.ts
@@ -1,13 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
 
-export const useOsmQuery = (osm_id: number) => {
+export const useOsmQuery = (osmId: number) => {
   const query = useQuery({
-    queryKey: ['osm/way', osm_id],
+    queryKey: ['osm/way', osmId],
     // https://wiki.openstreetmap.org/wiki/API_v0.6#Read:_GET_/api/0.6/[node|way|relation]/#id
     // https://tanstack.com/query/v4/docs/guides/query-functions#usage-with-fetch-and-other-clients-that-do-not-throw-by-default
     queryFn: async () => {
       const response = await fetch(
-        `https://www.openstreetmap.org/api/0.6/way/${osm_id}.json`
+        `https://www.openstreetmap.org/api/0.6/way/${osmId}.json`
       )
       if (!response.ok) {
         throw new Error('Network response was not ok')
