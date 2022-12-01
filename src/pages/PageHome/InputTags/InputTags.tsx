fa5c83628b4109ca96732e7a3381e7107f70c0b3
Tobias <tobias@fixmycity.de>
2022-12-01 05:57:03 +0100

    Form: Cleanup html

--- src/pages/PageHome/InputTags/InputTags.tsx
+++ src/pages/PageHome/InputTags/InputTags.tsx
@@ -22,25 +22,23 @@ export const InputTags: React.FC<Props> = ({ inputTags, onSubmit }) => {
   })
 
   return (
-    <>
-      <form className="" onSubmit={innerOnSubmit}>
-        <textarea
-          {...register('tags', { required: true })}
-          className="h-40 w-full resize rounded border bg-gray-50 font-mono text-sm"
-          defaultValue={inputTags?.join('\n')}
-        />
-        {errors.tags && (
-          <span className="inline-flex h-5 items-center bg-red-100 px-1 text-xs text-red-700">
-            Cannot be blank
-          </span>
-        )}
-        <button
-          type="submit"
-          className="flex h-5 items-center rounded border bg-gray-50 px-0.5 text-xs font-semibold shadow hover:bg-blue-50"
-        >
-          Update
-        </button>
-      </form>
-    </>
+    <form onSubmit={innerOnSubmit}>
+      <textarea
+        {...register('tags', { required: true })}
+        className="h-40 w-full resize rounded border bg-gray-50 font-mono text-sm"
+        defaultValue={inputTags?.join('\n')}
+      />
+      {errors.tags && (
+        <span className="inline-flex h-5 items-center bg-red-100 px-1 text-xs text-red-700">
+          Cannot be blank
+        </span>
+      )}
+      <button
+        type="submit"
+        className="flex h-5 items-center rounded border bg-gray-50 px-0.5 text-xs font-semibold shadow hover:bg-blue-50"
+      >
+        Update
+      </button>
+    </form>
   )
 }
